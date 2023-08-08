4b5a85724d13d0a3fdf5d02ff10997414fb1d643
igor camara <igorcamaras@gmail.com>
2023-08-07 22:53:29 -0300

    Initial commit

---
+++ EmailSender/Controllers/EmailController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MailKit.Net.Smtp;
+using EmailSender.Models;
+
+namespace EmailSender.Controllers
+{
+    public class EmailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(EmailModel model)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Your Name", model.ToEmail));
+            message.To.Add(new MailboxAddress("", model.ToEmail));
+            message.Subject = "Test Email";
+            message.Body = new TextPart("plain")
+            {
+                Text = "Email enviado com sucesso"
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect(model.SmtpServer, model.SmtpPort, false);
+                client.Authenticate(model.ToEmail, model.Password);
+                client.Send(message);
+                client.Disconnect(true);
+            }
+
+            return RedirectToAction("Index.cshtml");
+        }
+    }
+}
---
+++ EmailSender/Models/EmailModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EmailSender.Models
+{
+    public class EmailModel
+    {
+        public string ToEmail { get; set; }
+        public string Password { get; set; }
+        public string SmtpServer { get; set; }
+        public int SmtpPort { get; set; }
+    }
+
+}
